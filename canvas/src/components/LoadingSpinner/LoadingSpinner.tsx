c2567dd5866c798ffe1b7abf4373ec2c0ff4e876
rsbear <hellorosss@gmail.com>
2020-06-29 10:33:45 -0500

    Loading spinner size is now a string instead of number

--- canvas/src/components/LoadingSpinner/LoadingSpinner.tsx
+++ canvas/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import styled, { keyframes } from "styled-components";
 
 export interface LoadingSpinnerProps {
   color?: string;
-  size?: number;
+  size?: string;
   strokeWidth?: number;
 }
 
@@ -14,17 +14,17 @@ const spin = keyframes`
 
 const Spinner = styled.div<LoadingSpinnerProps>`
   display: flex;
-  height: ${(p) => (p.size ? p.size : "24px")};
-  width: ${(p) => (p.size ? p.size : "24px")};
+  height: ${p => (p.size ? p.size : "24px")};
+  width: ${p => (p.size ? p.size : "24px")};
   justify-content: center;
   align-items: center;
 
   .loader {
-    height: ${(p) => (p.size ? p.size : "24px")};
-    width: ${(p) => (p.size ? p.size : "24px")};
-    border: ${(p) => (p.strokeWidth ? p.strokeWidth : "2px")} solid
+    height: ${p => (p.size ? p.size : "24px")};
+    width: ${p => (p.size ? p.size : "24px")};
+    border: ${p => (p.strokeWidth ? p.strokeWidth : "2px")} solid
       rgba(0, 174, 239, 0.2);
-    border-top-color: ${(p) => (p.color ? p.color : "white")};
+    border-top-color: ${p => (p.color ? p.color : "white")};
     border-radius: 100%;
     animation: ${spin} 800ms infinite linear 0.25s;
   }
@@ -33,7 +33,7 @@ const Spinner = styled.div<LoadingSpinnerProps>`
 export const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
   color,
   size,
-  strokeWidth,
+  strokeWidth
 }) => (
   <Spinner color={color} size={size} strokeWidth={strokeWidth}>
     <div className="loader"></div>
